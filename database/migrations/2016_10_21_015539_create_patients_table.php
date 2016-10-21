641c6eb4a16826207423199058b1d4dedf0829dd
vagrant <vagrant@homestead>
2016-10-21 03:15:32 +0000

    create four migration for four table #1 patients #2 medical history #3 doctors #4appoinments

---
+++ database/migrations/2016_10_21_015539_create_patients_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePatientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('patients', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('IdKey')->unsigned()->index();
+            $table->string('name');
+            $table->integer('gender')->unsgned();
+            $table->string('bloodGroup');
+            $table->date('DOB');
+            $table->string('address');
+            $table->string('contact');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('patients');
+    }
+}
---
+++ database/migrations/2016_10_21_024354_create_appoinments_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAppoinmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('appoinments', function (Blueprint $table) {
+            $table->increments('id');
+            $table->dateTime('dateTime');
+            $table->integer('doctorId');
+            $table->integer('patientId');
+            $table->string('about');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('appoinments');
+    }
+}
