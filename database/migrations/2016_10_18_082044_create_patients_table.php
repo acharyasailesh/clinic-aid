c6222e9d246e8aa0c66501ca114c349acaba4593
sailesh acharya <meetluckyman@gmail.com>
2016-10-21 07:45:29 +0545

    Patient and Medical added

---
+++ database/migrations/2016_10_18_082044_create_patients_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePatientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+             
+
+
+        Schema::create('patients', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->string('name');
+            $table->integer('gender',1);
+            $table->string('bloodGroup',3);
+            $table->date('DOB');
+            $table->string('address');
+            $table->integer('contactNo');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('patients');
+    }
+}
